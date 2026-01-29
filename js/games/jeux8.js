e68dbff3edd57d1d8c2dbdba2ada110d818d1640
Toufik4991 <124460179+Toufik4991@users.noreply.github.com>
2026-01-29 20:24:27 +0100

    Update jeux8.js

--- js/games/jeux8.js
+++ js/games/jeux8.js
@@ -1,1 +1,12 @@
+function jeux1(container) {
+    container.innerHTML = `
+        <h2>📍 Point GPS 9</h2>
+        <p>Mini-jeu 8</p>
+        <button id="win">Continuer</button>
+    `;
+
+    document.getElementById("win").onclick = function () {
+        finishMiniGame(10);
+    };
+}
 

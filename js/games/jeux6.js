005be75036ec0719b548243a6f9d4e8317541cfd
Toufik4991 <124460179+Toufik4991@users.noreply.github.com>
2026-01-29 20:23:56 +0100

    Update jeux6.js

--- js/games/jeux6.js
+++ js/games/jeux6.js
@@ -1,1 +1,12 @@
+function jeux1(container) {
+    container.innerHTML = `
+        <h2>📍 Point GPS 7</h2>
+        <p>Mini-jeu 6</p>
+        <button id="win">Continuer</button>
+    `;
+
+    document.getElementById("win").onclick = function () {
+        finishMiniGame(10);
+    };
+}
 

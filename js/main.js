ff23e0dc0d716a1c923bf48f6b914fad65a2a1da
Toufik4991 <124460179+Toufik4991@users.noreply.github.com>
2026-01-29 19:58:18 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+function loadGameForStep(step, container) {
+    switch (step) {
+        case 0: jeux0(container); break;
+        case 1: jeux1(container); break;
+        case 2: jeux2(container); break;
+        case 3: jeux3(container); break;
+        case 4: jeux4(container); break;
+        case 5: jeux5(container); break;
+        case 6: jeux6(container); break;
+        case 7: jeux7(container); break;
+        case 8: jeux8(container); break;
+        default: showEndScreen();
+    }
+}
 

98686eb03493b3766d52e939a190a72b81c1437a
Toufik4991 <124460179+Toufik4991@users.noreply.github.com>
2026-01-29 19:56:48 +0100

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,17 @@
+let state = {
+    step: 0,
+    score: 0,
+    energy: 100,
+    name: "",
+    started: false
+};
+
+function saveState() {
+    localStorage.setItem("saumurQuest", JSON.stringify(state));
+}
+
+function loadState() {
+    const s = localStorage.getItem("saumurQuest");
+    if (s) state = JSON.parse(s);
+}
 

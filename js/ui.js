3654f3bddcd1135a98225a0dfc9677018cd127a7
Toufik4991 <124460179+Toufik4991@users.noreply.github.com>
2026-01-31 21:11:32 +0100

    Delete js directory

--- js/ui.js
+++
@@ -1,9 +0,0 @@
-function finishMiniGame(points) {
-    state.score += points;
-    state.energy = 100;
-    state.step++;
-    saveState();
-
-    const container = document.getElementById("game");
-    loadGameForStep(state.step, container);
-}
